189c7bc6d9e4cc968c4f40aa90f680029185b9a1
Christopher Ross <chris.rossx@gmail.com>
2015-09-24 13:42:38 -0700

    removed email from long_description

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-with open(path.join(here, 'DotStar_Emulator', 'emulator', 'media', 'about.txt')) as f:
-    long_description = f.read()
+long_description = """A simple emulator/simulator for the AdaFruit DotStar Strip Library.
+This project allows for quicker development of DotStar controller
+programs that you right, by not requiring you to have hardware
+hooked up to your device. For example, you can program a Raspberry
+pi controller from your normal PC using this.
 
+License: MIT
+Website: https://github.com/chrisrossx/DotStar_Emulator"""
 
 setup(
     name="DotStar_Emulator",
